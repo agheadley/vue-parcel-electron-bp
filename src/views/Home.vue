b38f1139b64e9878417b399ad53763c62911610f
agheadley <agheadley@gmail.com>
2020-07-08 09:02:54 +0100

    stable1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,19 @@
     <div>
         <p>Home.vue (Route: /)</p>
         <button @click="onClickGo">Go to /hello</button>
+    
+        <p>{{ message }}</p>
+    
+    
     </div>
 </template>
 
 <script>
   export default {
     name: 'Home',
+    data: {
+      message:'Hello from home ...'
+    },
     methods: {
       onClickGo() {
         this.$router.push('/hello')
